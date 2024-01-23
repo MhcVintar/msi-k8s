ab79f68480ff674e14e685d6d2602ddc3e5be07b
jakepel03 <jaka.pelko03@gmail.com>
2024-01-23 10:09:48 +0000

    Modified deleteScript

--- deleteScript.sh
+++ deleteScript.sh
@@ -7,10 +7,12 @@ kubectl delete service nginx
 kubectl delete service express
 
 # Delete Deployments
-kubectl delete deployments minio
-kubectl delete deployments postgres
 kubectl delete deployments nginx
 kubectl delete deployments express
+
+# Delete Statefulsets
+kubectl delete statefulset minio
+kubectl delete statefulset postgres
 
 # Delete Secrets and ConfigMap
 kubectl delete secret musify-secrets
@@ -19,11 +21,9 @@ kubectl delete configmap musify-configmap
 # Delete Ingress
 kubectl delete ingress musify-ingress
 
-# Delete PVs and PVCs
-kubectl delete pvc minio-storage-claim
-kubectl delete pvc postgres-storage-claim
-kubectl delete pv minio-storage-volume
-kubectl delete pv postgres-storage-volume
+# Delete PVs and PVCs (PVs deleted automatically with PVC deletion)
+kubectl delete pvc postgres-data-postgres-0
+kubectl delete pvc minio-storage-volume-minio-0
 
 # Delete Storage Classes
 kubectl delete sc minio-storage-class
